d4e18e9df698fb091edb0294764144afa8fccd8f
Abu Raihan <aburaihancse066@gmail.com>
2022-12-14 15:11:32 +0600

    Navigation routing done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navigation from './Components/Navigation';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
     <div className="App">
       <h3>React Router Dom</h3>
+      <Navigation />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>This Conact Page</h1>
+        </div>
+    )
+}
+
+export default Contact
